4119a51eaa8cb3ba6adc3f0183e99598b1b3059d
Faizan Jan <faizanjan619@gmail.com>
2023-05-25 14:44:46 +0530

    Separate the data and component for step 2

--- src/components/Step2.js
+++ src/components/Step2.js
@@ -1,26 +1,7 @@
 import Plan from "./Step2_Plans";
+import plans from "../data/plans" 
 
 let Step2 = () => {
-  const plans = [
-    {
-      id: "arcade",
-      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-arcade.svg",
-      monthlyPrice: 9,
-      checked: true,
-    },
-    {
-      id: "advanced",
-      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-advanced.svg",
-      monthlyPrice: 12,
-      checked: false,
-    },
-    {
-      id: "pro",
-      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-pro.svg",
-      monthlyPrice: 15,
-      cheched: false,
-    },
-  ];
   
   return (
     <form
---
+++ src/data/plans.js
@@ -0,0 +1,22 @@
+const plans = [
+    {
+      id: "arcade",
+      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-arcade.svg",
+      monthlyPrice: 9,
+      checked: true,
+    },
+    {
+      id: "advanced",
+      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-advanced.svg",
+      monthlyPrice: 12,
+      checked: false,
+    },
+    {
+      id: "pro",
+      img: "https://multi-step-form-sepia-six.vercel.app/assets/images/icon-pro.svg",
+      monthlyPrice: 15,
+      cheched: false,
+    },
+  ];
+
+  export default plans;
